fdc3f2afde2cd60ae845b5dc253e8edc676013cc
Cristina-santos89 <cris.manfredine@hormail.com>
2023-09-16 16:20:39 -0300

    criando o projeto para exemplo de tipos variaveis

---
+++ src/operadores/concatenacao.java
@@ -0,0 +1,18 @@
+package operadores;
+public class concatenacao {
+   
+public static void main(String[] args){
+    String concatenacao ="?"; 
+    System.out.println(concatenacao);
+    concatenacao = 1+1+1+"1";
+    System.out.println(concatenacao);
+    concatenacao = 1+"1"+1+1;
+    System.out.println(concatenacao);
+    concatenacao = 1+"1"+1+"1";
+    System.out.println(concatenacao);
+    concatenacao = "1"+1+1+1;
+    System.out.println(concatenacao);
+    concatenacao = "1"+(1+1+1);
+    System.out.println(concatenacao);
+    }  
+}
---
+++ src/operadores/concatenarString.java
@@ -0,0 +1,7 @@
+package operadores;
+public class concatenarString {
+    public static void main(String[] args){
+    String nomeCompleto = "LINGUAGEM" + " " + "JAVA"; //+ concatena string
+    System.out.print(nomeCompleto);
+    }
+}
