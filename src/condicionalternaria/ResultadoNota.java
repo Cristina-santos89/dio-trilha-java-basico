dd50ea3902665d11a2427d8f867fc46fbfcfc150
Cristina-santos89 <cris.manfredine@hormail.com>
2023-09-17 20:46:31 -0300

    criando o projeto para exemplo de tipos variaveis

---
+++ src/condicionalternaria/ResultadoNota.java
@@ -0,0 +1,9 @@
+package condicionalternaria;
+public class ResultadoNota {
+    public static void main(String[] args) {
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
+
---
+++ src/condicionalternaria/ResultadoNotaDois.java
@@ -0,0 +1,9 @@
+package condicionalternaria;
+public class ResultadoNotaDois {
+    public static void main(String[] args) {
+        int nota = 1;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
+
