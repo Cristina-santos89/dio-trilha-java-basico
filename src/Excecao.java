2addeb4633bce39d4db6ffb9600f75d876bde345
Cristina-santos89 <cris.manfredine@hormail.com>
2023-09-18 21:01:48 -0300

    criando o projeto para exemplo de tipos variaveis

---
+++ src/Excecao.java
@@ -0,0 +1,15 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+public class Excecao {
+        public static void main(String[] args) {
+        Number valor;
+        try {
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O formato da altura atende o padrão");
+        }
+    }
+}
+
